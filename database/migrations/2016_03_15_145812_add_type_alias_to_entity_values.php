888fea93bbe980c94662468399b6dcc129c546be
Jim Hollington <jim@bozboz.co.uk>
2016-03-16 10:45:30 +0000

    Add field_id to entity value migration

--- database/migrations/2016_03_15_145812_add_type_alias_to_entity_values.php
+++ database/migrations/2016_03_15_145812_add_type_alias_to_entity_values.php
@@ -13,23 +13,37 @@ class AddTypeAliasToEntityValues extends Migration
     public function up()
     {
         Schema::table('entity_values', function (Blueprint $table) {
-            $table->string('type_alias');
+            $table->integer('field_id')->after('id');
+            $table->string('type_alias')->after('field_id');
         });
 
         \DB::statement('
             UPDATE entity_values AS ev
-            SET type_alias = (
-                SELECT etf.type_alias
-                FROM entity_revisions AS er
-                JOIN entities AS e
-                ON e.id = er.entity_id
-                JOIN entity_templates AS et
-                ON et.id = e.template_id
-                JOIN entity_template_fields AS etf
-                ON etf.template_id = et.id
-                WHERE er.id = ev.revision_id
-                AND etf.name = ev.key
-            )
+            SET
+                type_alias = (
+                    SELECT etf.type_alias
+                    FROM entity_revisions AS er
+                    JOIN entities AS e
+                    ON e.id = er.entity_id
+                    JOIN entity_templates AS et
+                    ON et.id = e.template_id
+                    JOIN entity_template_fields AS etf
+                    ON etf.template_id = et.id
+                    WHERE er.id = ev.revision_id
+                    AND etf.name = ev.key
+                ),
+                field_id = (
+                    SELECT etf.id
+                    FROM entity_revisions AS er
+                    JOIN entities AS e
+                    ON e.id = er.entity_id
+                    JOIN entity_templates AS et
+                    ON et.id = e.template_id
+                    JOIN entity_template_fields AS etf
+                    ON etf.template_id = et.id
+                    WHERE er.id = ev.revision_id
+                    AND etf.name = ev.key
+                )
         ');
     }
 
@@ -42,6 +56,7 @@ class AddTypeAliasToEntityValues extends Migration
     {
         Schema::table('entity_values', function (Blueprint $table) {
             $table->dropColumn('type_alias');
+            $table->dropColumn('field_id');
         });
     }
 }
