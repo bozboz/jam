e2b6bed81cd43494566e5ce769d66ed6a711e3c1
Jim Hollington <jim@bozboz.co.uk>
2016-04-12 16:17:54 +0100

    Remove entitylistforeign field

--- src/Providers/JamServiceProvider.php
+++ src/Providers/JamServiceProvider.php
@@ -10,76 +10,76 @@ use Illuminate\Support\ServiceProvider;
 
 class JamServiceProvider extends ServiceProvider
 {
-	public function register()
-	{
-		$this->app->bind(
-			\Bozboz\Jam\Repositories\Contracts\EntityRepository::class,
-			\Bozboz\Jam\Repositories\EntityRepository::class
-		);
+    public function register()
+    {
+        $this->app->bind(
+            \Bozboz\Jam\Repositories\Contracts\EntityRepository::class,
+            \Bozboz\Jam\Repositories\EntityRepository::class
+        );
 
-		$this->app->bind(
-			\Bozboz\Jam\Entities\Contracts\LinkBuilder::class,
-			\Bozboz\Jam\Entities\LinkBuilder::class
-		);
+        $this->app->bind(
+            \Bozboz\Jam\Entities\Contracts\LinkBuilder::class,
+            \Bozboz\Jam\Entities\LinkBuilder::class
+        );
 
-		$this->app->singleton('EntityMapper', function ($app) {
-			return new Mapper;
-		});
-		Entity::setMapper($this->app['EntityMapper']);
+        $this->app->singleton('EntityMapper', function ($app) {
+            return new Mapper;
+        });
+        Entity::setMapper($this->app['EntityMapper']);
 
-		$this->app->singleton('FieldMapper', function ($app) {
-			return new Mapper;
-		});
-		Field::setMapper($this->app['FieldMapper']);
-	}
+        $this->app->singleton('FieldMapper', function ($app) {
+            return new Mapper;
+        });
+        Field::setMapper($this->app['FieldMapper']);
+    }
 
-	public function boot()
-	{
-		$packageRoot = __DIR__ . '/../../';
+    public function boot()
+    {
+        $packageRoot = __DIR__ . '/../../';
 
-		$this->loadViewsFrom("{$packageRoot}/resources/views", 'jam');
+        $this->loadViewsFrom("{$packageRoot}/resources/views", 'jam');
 
-		$this->publishes([
-			"{$packageRoot}database/migrations" => database_path('migrations')
-		], 'migrations');
+        $this->publishes([
+            "{$packageRoot}database/migrations" => database_path('migrations')
+        ], 'migrations');
 
-		$this->publishes([
-			"{$packageRoot}config/jam.php" => config_path('jam.php')
-		]);
+        $this->publishes([
+            "{$packageRoot}config/jam.php" => config_path('jam.php')
+        ]);
 
-		$permissions = $this->app['permission.handler'];
+        $permissions = $this->app['permission.handler'];
 
-		require __DIR__ . '/../permissions.php';
+        require __DIR__ . '/../permissions.php';
 
         $this->registerEntityTypes();
-		$this->registerFieldTypes();
+        $this->registerFieldTypes();
 
-		$this->buildAdminMenu();
+        $this->buildAdminMenu();
 
-		if (! $this->app->routesAreCached()) {
-			require "{$packageRoot}src/Http/routes.php";
-		}
-	}
+        if (! $this->app->routesAreCached()) {
+            require "{$packageRoot}src/Http/routes.php";
+        }
+    }
 
-	protected function buildAdminMenu()
-	{
-		$this->app['events']->listen('admin.renderMenu', function($menu)
-		{
-			$url = $this->app['url'];
+    protected function buildAdminMenu()
+    {
+        $this->app['events']->listen('admin.renderMenu', function($menu)
+        {
+            $url = $this->app['url'];
 
-			$entityTypes = $this->app['EntityMapper']->getAll()->each(function($type, $alias) use ($url, $menu) {
-				if ($type->templates()->count()) {
-					$type->addToMenu($menu, $url);
-				}
-			});
+            $entityTypes = $this->app['EntityMapper']->getAll()->each(function($type, $alias) use ($url, $menu) {
+                if ($type->templates()->count()) {
+                    $type->addToMenu($menu, $url);
+                }
+            });
 
-			if ($menu->gate('manage_entities')) {
-				$menu['Jam'] = [
-					'Types' => $url->route('admin.entity-types.index'),
-				];
-			}
-		});
-	}
+            if ($menu->gate('manage_entities')) {
+                $menu['Jam'] = [
+                    'Types' => $url->route('admin.entity-types.index'),
+                ];
+            }
+        });
+    }
 
     protected function registerEntityTypes()
     {
@@ -87,36 +87,34 @@ class JamServiceProvider extends ServiceProvider
 
         $mapper->register([
             'pages' => new \Bozboz\Jam\Types\NestedType([
-            	'name' => 'Pages',
-            	'entity' => \Bozboz\Jam\Entities\SortableEntity::class,
-            	'link_builder' => function() {
-            		return app(\Bozboz\Jam\Entities\Contracts\LinkBuilder::class);
-            	}
+                'name' => 'Pages',
+                'entity' => \Bozboz\Jam\Entities\SortableEntity::class,
+                'link_builder' => function() {
+                    return app(\Bozboz\Jam\Entities\Contracts\LinkBuilder::class);
+                }
             ]),
         ]);
     }
 
-	protected function registerFieldTypes()
-	{
-		$mapper = $this->app['FieldMapper'];
+    protected function registerFieldTypes()
+    {
+        $mapper = $this->app['FieldMapper'];
 
-		$mapper->register([
-			'text'                     => \Bozboz\Jam\Fields\Text::class,
-			'textarea'                 => \Bozboz\Jam\Fields\Textarea::class,
-			'htmleditor'               => \Bozboz\Jam\Fields\HTMLEditor::class,
-			'image'                    => \Bozboz\Jam\Fields\Image::class,
-			'gallery'                  => \Bozboz\Jam\Fields\Gallery::class,
-			'date'                     => \Bozboz\Jam\Fields\Date::class,
-			'date-time'                => \Bozboz\Jam\Fields\DateTime::class,
-			'toggle'                   => \Bozboz\Jam\Fields\Toggle::class,
-			// 'foreign'                  => \Bozboz\Jam\Fields\Foreign::class,
-			'parent-entity'            => \Bozboz\Jam\Fields\ParentEntity::class,
-			'entity-list'              => \Bozboz\Jam\Fields\EntityList::class,
-			'entity-list-foreign'      => \Bozboz\Jam\Fields\EntityListForeign::class,
-			'belongs-to-type'          => \Bozboz\Jam\Fields\BelongsToType::class,
-			'belongs-to-entity'        => \Bozboz\Jam\Fields\BelongsToEntity::class,
-			'belongs-to-many-entities' => \Bozboz\Jam\Fields\BelongsToManyEntities::class,
-			'hidden'                   => \Bozboz\Jam\Fields\Hidden::class,
-		]);
-	}
+        $mapper->register([
+            'text'                     => \Bozboz\Jam\Fields\Text::class,
+            'textarea'                 => \Bozboz\Jam\Fields\Textarea::class,
+            'htmleditor'               => \Bozboz\Jam\Fields\HTMLEditor::class,
+            'image'                    => \Bozboz\Jam\Fields\Image::class,
+            'gallery'                  => \Bozboz\Jam\Fields\Gallery::class,
+            'date'                     => \Bozboz\Jam\Fields\Date::class,
+            'date-time'                => \Bozboz\Jam\Fields\DateTime::class,
+            'toggle'                   => \Bozboz\Jam\Fields\Toggle::class,
+            'parent-entity'            => \Bozboz\Jam\Fields\ParentEntity::class,
+            'entity-list'              => \Bozboz\Jam\Fields\EntityList::class,
+            'belongs-to-type'          => \Bozboz\Jam\Fields\BelongsToType::class,
+            'belongs-to-entity'        => \Bozboz\Jam\Fields\BelongsToEntity::class,
+            'belongs-to-many-entities' => \Bozboz\Jam\Fields\BelongsToManyEntities::class,
+            'hidden'                   => \Bozboz\Jam\Fields\Hidden::class,
+        ]);
+    }
 }
