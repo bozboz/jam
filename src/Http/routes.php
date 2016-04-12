b317603acf611786f581452518bf3e0b57433239
Sam King <sdking02@gmail.com>
2016-04-12 15:05:43 +0100

    Use route groups for clarity

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -9,19 +9,22 @@
 Route::group(array('middleware' => 'web', 'namespace' => 'Bozboz\Jam\Http\Controllers\Admin', 'prefix' => 'admin'), function() {
 
 	Route::resource('entities', 'EntityController', ['except' => ['index', 'create']]);
-	Route::get('entities/{type}/create', 'EntityController@createOfType');
-	Route::post('entities/{type}/publish', 'EntityController@publish');
-	Route::post('entities/{type}/unpublish', 'EntityController@unpublish');
-	Route::post('entities/{type}/schedule', 'EntityController@schedule');
+	Route::group(['prefix' => 'entities/{type}'], function()
+	{
+		Route::get('create', 'EntityController@createOfType');
+		Route::post('publish', 'EntityController@publish');
+		Route::post('unpublish', 'EntityController@unpublish');
+		Route::post('schedule', 'EntityController@schedule');
+	});
+
+	Route::get('entities/{id}/revisions', 'EntityRevisionController@indexForEntity');
+	Route::post('entities/{id}/revisions/revert', 'EntityRevisionController@revert');
 
 	Route::resource('entity-list', 'EntityListController', ['except' => ['create']]);
 	Route::get('entity-list/{type}/{parent_id}/create', [
 		'uses' => 'EntityListController@createForEntityListField',
 		'as' => 'admin.entity-list.create-for-list'
 	]);
-
-	Route::get('entities/{id}/revisions', 'EntityRevisionController@indexForEntity');
-	Route::post('entities/{id}/revisions/revert', 'EntityRevisionController@revert');
 
 	Route::resource('entity-types', 'EntityTypeController');
 
