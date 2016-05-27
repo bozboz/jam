07f45380f2d2425304faf2fcbb4660e72e288b65
Jim Hollington <jim@bozboz.co.uk>
2016-05-27 09:34:42 +0100

    Reimplement the updating of paths upon sorting entities

--- src/Entities/SortableEntity.php
+++ src/Entities/SortableEntity.php
@@ -7,15 +7,23 @@ use Bozboz\Admin\Base\Sorting\Sortable;
 
 class SortableEntity extends Entity implements Sortable
 {
-	use NestedSortableTrait;
+    use NestedSortableTrait {
+        sort as traitSort;
+    }
 
-	public function sortBy()
-	{
-		return '_lft';
-	}
+    public function sortBy()
+    {
+        return '_lft';
+    }
 
-	public function scopeOrdered($query)
-	{
-		$query->defaultOrder();
-	}
+    public function scopeOrdered($query)
+    {
+        $query->defaultOrder();
+    }
+
+    public function sort($before, $after, $parent)
+    {
+        $this->traitSort($before, $after, $parent);
+        static::$linkBuilder->updatePaths($this);
+    }
 }
