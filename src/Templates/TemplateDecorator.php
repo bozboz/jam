b0d456cb64ac3c0e0719b3e4aaa8bda8e04ab9a5
Sam King <sdking02@gmail.com>
2016-07-28 15:43:17 +0100

    Replace free text "view" with select on template

--- src/Templates/TemplateDecorator.php
+++ src/Templates/TemplateDecorator.php
@@ -4,10 +4,12 @@ namespace Bozboz\Jam\Templates;
 
 use Bozboz\Admin\Base\ModelAdminDecorator;
 use Bozboz\Admin\Fields\HiddenField;
+use Bozboz\Admin\Fields\SelectField;
 use Bozboz\Admin\Fields\TextField;
 use Bozboz\Jam\Templates\Template;
 use Bozboz\Jam\Types\Type;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Input;
 
 class TemplateDecorator extends ModelAdminDecorator
@@ -37,11 +39,25 @@ class TemplateDecorator extends ModelAdminDecorator
 		return [
 			new TextField('name'),
 			($instance->exists ? new TextField('alias') : null),
-			new TextField('view'),
+			new SelectField('view', ['options' => $this->getViews()]),
 			new TextField('listing_view'),
 			new TextField('listing_fields'),
 			new HiddenField('type_alias')
 		];
+	}
+
+	protected function getViews()
+	{
+		$path = base_path('resources/views');
+
+		return collect(File::allFiles($path))->map(function($file) use ($path) {
+			$remove = [$path . '/', '.blade.php'];
+			return str_replace($remove, '', $file->getPathname());
+		})->filter(function($file) {
+			return preg_match('/^styleguide/', $file) !== 1;
+		})->keyBy(function($value) {
+			return str_replace('/', '.', $value);
+		})->prepend('-', '')->all();
 	}
 
 	protected function modifyListingQuery(Builder $query)
