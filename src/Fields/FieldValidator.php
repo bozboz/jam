deb4c1d2bc7db4bbd33f301fa90744c9726ccdb8
Jim Hollington <jim@bozboz.co.uk>
2016-10-17 16:16:41 +0100

    Allow numbers in field names

--- src/Fields/FieldValidator.php
+++ src/Fields/FieldValidator.php
@@ -7,7 +7,7 @@ use Bozboz\Admin\Services\Validators\Validator;
 class FieldValidator extends Validator
 {
 	protected $rules = [
-		'name' => 'required|regex:/^[a-z_]+$/',
+		'name' => 'required|regex:/^[a-z0-9_]+$/',
 		'type_alias' => 'required',
 		'template_id' => 'required|exists:entity_templates,id'
 	];
