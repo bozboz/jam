828eef180936018542874775468f3312b779f7d6
Tom Humphris <tom@bozboz.co.uk>
2017-08-29 14:12:59 +0100

    Add ability to publish expired entity list items from listing

--- resources/views/admin/partials/entity-list-item.blade.php
+++ resources/views/admin/partials/entity-list-item.blade.php
@@ -1,7 +1,7 @@
 <li class="js-nested-item grid__item small-12 medium-4 large-3 xlarge-2" data-id="{{ $entity->id }}">
     <div class="cards__item sorting-handle js-sorting-handle">
         <h4>{{ $entity->name }}</h4>
-        @if (!$entity->status)
+        @if ( ! $entity->status)
             <div class="panel panel-danger"><div class="panel-heading">
                 Hidden
             </div></div>
@@ -18,9 +18,19 @@
                 <abbr title="{{ $entity->currentRevision->formatted_expired_at }}">Expired</abbr>
             </div></div>
         @endif
-        <a href="{{ route('admin.entity-list.edit', [$entity->id]) }}" class="cards__btn btn btn-info btn-sm">
-            <i class="fa-pencil fa"></i> Edit
-        </a>
+
+        <div class="cards__btn">
+            <a href="{{ route('admin.entity-list.edit', [$entity->id]) }}" class="btn btn-info btn-sm">
+                <i class="fa-pencil fa"></i> Edit
+            </a>
+
+            @if ($entity->status === Bozboz\Jam\Entities\Revision::EXPIRED)
+                <a href="{{ route('admin.entities.publish', [$entity->id]) }}" class="btn btn-success btn-sm js-publish-entity-btn">
+                    <i class="fa-save fa"></i> Publish
+                </a>
+            @endif
+        </div>
+
         <a class="cards__btn--delete js-delete-entity-btn btn btn-sm btn-danger" type="submit" href="#">
             <i class="fa-trash fa"></i> Delete
         </a>
