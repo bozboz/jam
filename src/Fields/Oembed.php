c20c42e872d8d652caa9045f68437739988744ef
oddvalue <jim@oddvalue.co.uk>
2021-12-20 13:36:42 +0000

    Fix youtube thumbnail on oembed field

--- src/Fields/Oembed.php
+++ src/Fields/Oembed.php
@@ -33,13 +33,17 @@ class Oembed extends Text
             return false;
         }
 
-        $embed = <<<HTML
+        $embed = Embed::create($url);
+
+        $code = <<<HTML
         <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/{$videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 HTML;
 
         return (object)[
-            'code' => $embed,
+            'code' => $code,
+            'image' => $embed->image,
         ];
+
     }
 
     protected function getCacheKey($value)
